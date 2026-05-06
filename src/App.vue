6afd7d3e686949d66adf34b2e2a11616ad986b7d
chris <Christopher.Mensah@Student.HTW-Berlin.de>
2026-05-06 20:03:36 +0200

    Milestone 2 F

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import ExerciseList from './ExerciseList.vue'
+
+type Exercise = {
+  title: string
+  muscleGroup: string
+  date: string
+  reps: number
+}
+
+const exercises = ref<Exercise[]>([])
+
+onMounted(async () => {
+  const response = await fetch('http://localhost:8080/fitnesstrainer')
+  exercises.value = await response.json()
+})
+</script>
+ç
+<template>
+  <main>
+    <h1>Fitness Trainer</h1>
+
+    <ExerciseList :exercises="exercises" />
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ src/ExerciseList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  exercises: {
+    title: string
+    muscleGroup: string
+    date: string
+    reps: number
+  }[]
+}>()
+</script>
+
+<template>
+  <ul>
+    <li v-for="exercise in exercises" :key="exercise.title">
+      <strong>{{ exercise.title }}</strong
+      ><br />
+      Muskelgruppe: {{ exercise.muscleGroup }}<br />
+      Datum: {{ exercise.date }}<br />
+      Wiederholungen: {{ exercise.reps }}
+    </li>
+  </ul>
+</template>
